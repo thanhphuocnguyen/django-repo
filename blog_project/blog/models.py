d2e4d72aeb1b5db77a50fb289632dbe4598f817e
Phuoc Nguyen <thanhphuocb7@gmail.com>
2022-08-27 00:03:12 +0700

    add app blog

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,43 @@
+from pyexpat import model
+from turtle import title
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(default=timezone.now())
+    published_date = models.DateField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.content
